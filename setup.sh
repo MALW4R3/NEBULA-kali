76c675e52c5ace8e77ecb96d1abc18910cd193c7
APT-GET47 <57804710+MALW4R3@users.noreply.github.com>
2020-06-18 16:41:21 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+pip2 install requests
+apt-get install fonts-thai-tlwg
+mv update-nebula /bin
+cd
+cd ..
+cd ..
+cd bin
+chmod +x update-nebula
+cd
+cd NEBULA-kali
+git clone https://github.com/LOoLzeC/SH33LL
+git clone https://github.com/hahwul/XSpear
+cd XSpear
+gem install XSpear-1.4.1.gem
+cd ..
+mv nebula /bin
+cd
+cd ..
+cd ..
+cd bin
+chmod +x nebula
+cd
+cd NEBULA-kali
+clear
+cat finished.txt | lolcat
